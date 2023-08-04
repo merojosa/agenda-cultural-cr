b547861a7d5234271a21390a6407398de904bc3f
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-08-03 22:12:13 -0600

    Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Agenda Cultural CR - Teatro🎭</title>
+	<meta name="description" content="Cartelera de teatro para Costa Rica" />
+</svelte:head>
+
+<div class="app">
+	<header>
+		<h1>Agenda Cultural CR - Teatro🎭</h1>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>Creado por Jose Andrés</p>
+	</footer>
+</div>
+
+<style>
+	:global(body) {
+		all: unset;
+	}
+
+	.app {
+		all: unset;
+		min-height: 100vh;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: auto 1fr auto;
+		grid-template-areas: 'header' 'main' 'footer';
+		background-color: var(--primaryColor);
+		font-family: 'Open Sans', sans-serif;
+		overflow-x: hidden;
+	}
+
+	header {
+		grid-area: header;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+	}
+
+	main {
+		width: 75rem;
+		margin: 2rem auto;
+	}
+
+	footer {
+		grid-area: footer;
+		display: flex;
+		justify-content: center;
+	}
+</style>
