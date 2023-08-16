120264a60f7ce5d1beb96d5c575c78dd55d2f4b7
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-08-16 16:58:57 -0600

    Monorepo with turborepo

---
+++ apps/backend/src/scraping/theater/update-theater-data.ts
@@ -0,0 +1,23 @@
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
+import { getTeatroNacionalData } from "./teatro-nacional";
+
+export async function updateTheaterData() {
+  const browser = await puppeteer.launch({
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+    args: [
+      ...chromium.args,
+      "--hide-scrollbars",
+      "--disable-web-security",
+      "--use-gl=swiftshader",
+    ],
+    executablePath: await chromium.executablePath(),
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+
+  await getTeatroNacionalData(page);
+
+  await browser.close();
+}
