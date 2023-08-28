c14233e16e3cc67504902e5086f4f1d50628617b
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-08-27 20:41:46 -0600

    Change cheerio to html-to-text

--- apps/backend/src/utils/util.scraping.ts
+++ apps/backend/src/utils/util.scraping.ts
@@ -1,4 +1,4 @@
-import * as cheerio from 'cheerio';
+import { compile } from 'html-to-text';
 
 export const spanishMonths: Record<string, number> = {
 	enero: 1,
@@ -15,12 +15,8 @@ export const spanishMonths: Record<string, number> = {
 	diciembre: 12,
 } as const;
 
+const compiledConvert = compile({ preserveNewlines: true, wordwrap: null });
+
 export function htmlToPlainText(html: string): string {
-	// Load the HTML string into a Cheerio object
-	const $ = cheerio.load(html);
-
-	// Use Cheerio's text() method to extract plain text
-	const plainText = $.text();
-
-	return plainText;
+	return compiledConvert(html);
 }
