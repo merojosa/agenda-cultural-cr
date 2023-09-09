3645dc5bc4e1b2dbc5ed5b3fcd5e916b19fb1194
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-09-09 13:34:42 -0600

    Filter activities only for months

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,7 +1,10 @@
 import { activityTable } from 'db-schema';
 import { db } from '$lib/server/db';
+import { between } from 'drizzle-orm';
+import { DateTime } from 'luxon';
 
 export async function load() {
+	const today = DateTime.local().startOf('day');
 	const activities = await db
 		.select({
 			title: activityTable.title,
@@ -10,6 +13,15 @@ export async function load() {
 			imageUrl: activityTable.imageUrl,
 			activityUrl: activityTable.activityUrl,
 		})
-		.from(activityTable);
+		.from(activityTable)
+		.where(
+			between(
+				activityTable.datetime,
+				today.toJSDate(),
+				today.plus({ months: 1 }).endOf('day').toJSDate()
+			)
+		)
+		.orderBy(activityTable.datetime);
+
 	return { activities };
 }
