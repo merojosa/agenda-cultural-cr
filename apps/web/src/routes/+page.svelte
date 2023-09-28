095d9a3b59e68e9f6e07b2f42ce109085e5add45
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-09-28 00:08:18 -0600

    Remove button, improve clamp

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -31,32 +31,31 @@
 
 <section class="grid grid-cols-3 gap-12">
 	{#each data.activities as activity}
-		<Card.Root tag="article" class="relative flex flex-col h-[34rem]">
-			<img
-				class="w-full h-1/2 object-cover rounded-ss-lg rounded-se-lg"
-				src={activity.imageUrl}
-				alt={`Portada de ${activity.title}`}
-			/>
-			<Card.Content class="h-1/2 p-4 flex flex-col gap-3">
-				<Card.Title class="line-clamp-3" tag="h3">{activity.title}</Card.Title>
-
-				<Card.Description class="line-clamp-[6]"
-					>{formatDescription(activity.description)}</Card.Description
-				>
-
+		<Card.Root tag="article" class="relative flex flex-col h-[28rem]">
+			<a class="w-full h-1/2" href="/a">
+				<img
+					class="w-full h-full object-cover rounded-ss-lg rounded-se-lg"
+					src={activity.imageUrl}
+					alt={`Portada de ${activity.title}`}
+				/>
 				<time
 					class="absolute right-[-22px] top-[-21px] bg-secondary text-black p-3 rounded-sm"
 					datetime={activity.datetime.toISOString()}>{formatDatetime(activity.datetime)}</time
 				>
+			</a>
+			<Card.Content class="h-1/2 p-4 flex flex-col gap-3">
+				<a href="/a">
+					<Card.Title
+						class={`${activity.title.length > 35 ? 'line-clamp-2' : 'line-clamp-1'} leading-normal`}
+						tag="h2">{activity.title}</Card.Title
+					>
+				</a>
 
-				<Button
-					class="w-fit mt-auto mx-auto text-lg"
-					target="_blank"
-					href={activity.activityUrl}
-					variant="link"
+				<Card.Description
+					class={`leading-normal ${
+						activity.title.length > 35 ? 'line-clamp-6' : 'line-clamp-[7]'
+					} text-ellipsis`}>{formatDescription(activity.description)}</Card.Description
 				>
-					Sitio oficial
-				</Button>
 			</Card.Content>
 		</Card.Root>
 	{/each}
