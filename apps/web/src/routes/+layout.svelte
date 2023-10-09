c4df7477eee9618441fb1125b5f5357f8672b5d8
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-10-09 01:40:22 -0600

    Improve header and footer

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,20 +1,30 @@
 <script>
+	import { Button } from '$lib/components/ui/button';
 	import '../app.css';
 </script>
 
 <div
 	class="app bg-surface grid min-h-screen grid-cols-1 grid-rows-[auto_1fr_auto] overflow-x-hidden"
 >
-	<header class="flex w-screen justify-center">
-		<h1>Agenda Cultural CR</h1>
+	<header class="flex w-screen justify-center py-10">
+		<Button variant="link" class="text-foreground w-fit p-0 text-3xl font-medium" href="/">
+			<h1>Agenda Cultural CR</h1>
+		</Button>
 	</header>
 
-	<main class="mx-auto my-8 w-[75rem]">
+	<main class="mx-auto w-[75rem] pb-24 pt-8">
 		<slot />
 	</main>
 
-	<footer class="flex justify-center">
-		<p>Creado por Jose Andrés</p>
+	<footer class="flex justify-center border-t py-6">
+		<p>
+			Creado con ❤️ por <Button
+				href="https://github.com/merojosa/"
+				target="_blank"
+				variant="link"
+				class="w-fit p-0 text-base">Jose Andrés</Button
+			>
+		</p>
 	</footer>
 </div>
 
