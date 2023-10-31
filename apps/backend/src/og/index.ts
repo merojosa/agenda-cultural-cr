6f4893daa7efa38589dba581460271090d0b96be
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-10-31 00:38:39 -0600

    Html

--- apps/backend/src/og/index.ts
+++ apps/backend/src/og/index.ts
@@ -2,14 +2,16 @@ import type { APIGatewayEvent, Callback, Context } from 'aws-lambda';
 import satori from 'satori';
 import { html } from 'satori-html';
 import sharp from 'sharp';
-
+import fs from 'node:fs/promises';
 export const generateOG = async (event: APIGatewayEvent, _: Context, callback: Callback) => {
 	if (event.queryStringParameters && event.queryStringParameters['title']) {
 		const title = event.queryStringParameters['title'];
 		console.log('BREAKPOINT title', title);
 	}
 
-	const markup = html`<div style="color: black;">hello, world</div>`;
+	const content = (await fs.readFile('templates/basic.html')).toString('utf-8');
+
+	const markup = html(content);
 
 	const fontFile = await fetch('https://og-playground.vercel.app/inter-latin-ext-700-normal.woff');
 	const fontData = await fontFile.arrayBuffer();
